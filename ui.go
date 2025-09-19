eb7d9641fbe13c3525105e58901f3d53444cd8bd
ukerem <kerem.uysal@ynov.com>
2025-09-19 09:41:10 +0200

    change ui.go

--- ui.go
+++ ui.go
@@ -6,7 +6,6 @@ import (
 )
 
 func SimpleBanner() {
-	// Bannière ASCII de base
 	banner := []string{
 		" ####   #   #  ####   #####  ####   #   #  #   #   ###   #   #",
 		"#         #    #   #  #      #   #   # #   ##  #  #   #  #   #",
@@ -16,10 +15,9 @@ func SimpleBanner() {
 		"                 C  Y  B  E  R  Y  N  O  V",
 	}
 
-	// Affiche chaque ligne une par une avec une petite pause
 	for _, line := range banner {
 		fmt.Println(line)
-		time.Sleep(200 * time.Millisecond) // pause 0,2 seconde
+		time.Sleep(200 * time.Millisecond) 
 	}
 }
 
